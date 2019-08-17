16009ba28785375e7094f35d53996dda2f81c13e
simongarisch <gatman946@gmail.com>
2019-08-17 14:17:07 +1000

    Cleaning up setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,38 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+setup_requirements = ["pytest-runner"]
 
-requirements = [ ]
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Simon Garisch",
-    author_email='gatman946@gmail.com',
+    author_email="gatman946@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Python racing bot for https://play.typeracer.com/",
-    install_requires=requirements,
+    install_requires=["selenium>=3.141.0", "beautifulsoup4>=4.8.0"],
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
-    keywords='pytyperacer',
-    name='pytyperacer',
-    packages=find_packages(include=['pytyperacer']),
+    keywords="pytyperacer",
+    name="pytyperacer",
+    packages=find_packages(include=["pytyperacer"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/simongarisch/pytyperacer',
-    version='0.1.0',
+    url="https://github.com/simongarisch/pytyperacer",
+    version="0.1.0",
     zip_safe=False,
 )
