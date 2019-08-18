bdac193c6f7d0011c3356c7fb1987a9e2c3a3475
simongarisch <gatman946@gmail.com>
2019-08-19 07:00:35 +1000

    Adding to settings: race again and html login.

--- pytyperacer/settings.py
+++ pytyperacer/settings.py
@@ -6,14 +6,14 @@ from enum import Enum
 
 URL = "https://play.typeracer.com/"
 
-MAX_WAIT_SECONDS = 5
+MAX_WAIT_SECONDS = 20
+CHARS_PER_WORD = 5  # typeracer counts X characers as a word
+
+HTML_SIGN_IN = '<a class="gwt-Anchor" href="javascript:;">Sign In</a>'
 
 CSS_SELECTOR_LOGIN = "a.gwt-Anchor"
-# CSS_SELECTOR_LOGIN = "#tstats > table > tbody > tr.datarow > td:nth-child(1) > table > tbody > tr > td:nth-child(1) > a"
-# CSS_SELECTOR_SIGN_OUT = (
-#    '<a class="gwt-Anchor" href="javascript:;">Sign Out</a>'
-# )
 CSS_SELECTOR_ENTER_RACE = ".mainMenu  a.gwt-Anchor"
+CSS_SELECTOR_RACE_AGAIN = ".raceAgainLink"
 CSS_SELECTOR_USER = "input.gwt-TextBox"
 CSS_SELECTOR_PASS = "input.gwt-PasswordTextBox"
 CSS_SELECTOR_BTN_LOGIN = "button.gwt-Button"
@@ -22,7 +22,7 @@ CSS_SELECTOR_RACING = "input.txtInput"
 CSS_SELECTORS = [
     CSS_SELECTOR_LOGIN,
     CSS_SELECTOR_ENTER_RACE,
-    # CSS_SELECTOR_SIGN_OUT,
+    CSS_SELECTOR_RACE_AGAIN,
     CSS_SELECTOR_USER,
     CSS_SELECTOR_PASS,
     CSS_SELECTOR_BTN_LOGIN,
