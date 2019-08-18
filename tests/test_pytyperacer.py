ac2eb5a941bb66755ce3256033e286ef61250f07
simongarisch <gatman946@gmail.com>
2019-08-19 07:01:25 +1000

    Tweaking tests

--- tests/test_pytyperacer.py
+++ tests/test_pytyperacer.py
@@ -2,8 +2,9 @@ from pytyperacer import TypingBot
 
 USERNAME = "typeracer196"
 PASSWORD = "typeracermail"
+WPM = 100
 
 
 def test_pytyperacer():
-    bot = TypingBot(USERNAME, PASSWORD)
-    bot.race()
+    bot = TypingBot(USERNAME, PASSWORD, WPM)
+    bot.race(max_actions=10)
