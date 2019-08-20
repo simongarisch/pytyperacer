51773efb706c6cb7c4d4ceccd8f3f27ba7a13599
simongarisch <gatman946@gmail.com>
2019-08-21 06:56:19 +1000

    Adjusted testing wpm and max_actions.

--- tests/test_pytyperacer.py
+++ tests/test_pytyperacer.py
@@ -2,9 +2,9 @@ from pytyperacer import TypingBot
 
 USERNAME = "typeracer196"
 PASSWORD = "typeracermail"
-WPM = 100
+WPM = 85
 
 
 def test_pytyperacer():
     bot = TypingBot(USERNAME, PASSWORD, WPM)
-    bot.race(max_actions=10)
+    bot.race(max_actions=5)
